b003868a576b32862ec489cfb7d00d3b0ce7e3fe
symnbe <ookaminingen@me.com>
2023-02-03 23:59:59 +0900

    [Fix]books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,27 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if book.save
+      redirect_to book_path(@book), notice: "You have created book successfully"
+    else
+      @books = Book.all
+      render "index"
+    end
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
 end
