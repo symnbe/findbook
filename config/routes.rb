d81f8d99ae64e21858a62bfe99f695d13f0daf4c
symnbe <ookaminingen@me.com>
2023-03-13 23:59:50 +0900

    [Add]Fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   resources :books, only: [:index, :show, :edit, :update, :destroy]
   resources :users, only: [:indedx, :show, :edit, :update,]
 end
