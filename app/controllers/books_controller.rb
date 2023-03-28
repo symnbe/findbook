005ae39491725e3262a8a96f93aee155e9033c68
symnbe <ookaminingen@me.com>
2023-03-28 11:57:05 +0000

    [Add]error_html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully"
     else
       @books = Book.all
-      render "index"
+      render :index
     end
   end
 
@@ -24,12 +24,12 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     if @book.user == current_user
-      render "edit"
+      render :index
     else
       redirect_to books_path
     end
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
