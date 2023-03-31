e8325e164beba13d1abb328d103e54f5fad49b3e
symnbe <ookaminingen@me.com>
2023-03-31 06:29:48 +0000

    [Add]Comment機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update,]
