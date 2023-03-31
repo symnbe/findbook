81e4d88aceae70a68aa4a6a3e39acd0184680c6c
symnbe <ookaminingen@me.com>
2023-03-31 04:45:09 +0000

    [fix]いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.save
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_to book_path(book)
   end
 
   def destroy
-    @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
-    @book_favorite.destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
 
 end
